b6ba3adc224490b143c783b7190b133ce478904a
jhonatan Salas <128430511+JHOMASA@users.noreply.github.com>
2025-05-23 14:53:39 -0500

    app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import streamlit as st
 import pandas as pd
 import sqlite3
 from datetime import datetime, timedelta
+from graphviz import Diagraph
 import pytz
 import hashlib
 import matplotlib.pyplot as plt
